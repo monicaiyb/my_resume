3bafa343c1b3f0cda9b9b0c61b11ad4c843b2965
monicaiyb <monicaiyb@gmail.com>
2021-08-20 02:18:22 -0700

    Add index route

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT|| 3000
 
-
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 app.get('/resume', function (req, res) {
-    res.send('index.html')
+    res.sendFile('index.html', { root: __dirname });
   })
   
 
