e724c8ffe64daa491d06c54a90e1313cd3f11aea
monicaiyb <monicaiyb@gmail.com>
2021-08-20 01:44:03 -0700

    Add Procfile

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT|| 3000
 
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 app.get('/resume', function (req, res) {
     res.send('index.html')
   })
+  
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${PORT}`)
