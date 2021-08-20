b7676bf36ff85eb5a9ba6066532d352bbbffe576
monicaiyb <monicaiyb@gmail.com>
2021-08-20 01:25:31 -0700

    Change port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const PORT = process.env.PORT|| 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -10,5 +10,5 @@ app.get('/resume', function (req, res) {
   })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${PORT}`)
 })
